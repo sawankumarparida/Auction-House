6edaa8369d0b683734b133772c5b82f17ff992e4
skpar <skpar@S_K_PARIDA>
2025-02-10 03:50:17 +0530

    Add project files.

---
+++ Auctions/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using Auctions.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Auctions.Controllers
+{
+    public class HomeController(ILogger<HomeController> logger) : Controller
+    {
+        private readonly ILogger<HomeController> _logger = logger;
+
+        public ILogger<HomeController> Logger => _logger;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
